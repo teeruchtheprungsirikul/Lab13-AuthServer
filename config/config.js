fabff88b0540c3baf7efca98386705b6596f1a2c
teeruchtheprungsirikul <teeruch_th@hotmail.com>
2022-04-18 15:30:04 -0700

    Add AuthServer

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    jwtSecret: 'mobile_application_token_secret_key',
+    db: 'mongodb+srv://admin:1234@cluster0.0bo1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
